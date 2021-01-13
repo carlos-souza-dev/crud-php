d8ffee4604d6ad955ea941c44bf59d240f1b6d3d
carlos-souza-02 <carlos.alberto.g.s@icloud.com>
2021-01-13 20:22:07 -0300

    add validation for routers

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\Fornecedor;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 use DB;
 
 class ApiController extends Controller {
@@ -14,29 +16,90 @@ class ApiController extends Controller {
     }
 
     public function list () {        
+        
         $fornecedores = $this->model->all();
-        return response()->json($fornecedores);
-    }
 
-    public function search ($id) {
-        $fornecedor = $this->model->find($id);
-        return response()->json($fornecedor);
+        try{
+            if(count($fornecedores)){
+                return response()->json($fornecedores, Response::HTTP_OK);
+            } else {
+                return response()->json([], Response::HTTP_OK);
+            }
+        }catch (QueryException $exception){
+            return response()->json(['error'=>'Erro de conexão com o banco de dados'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function create(Request $request) {
-        $fornecedor = $this->model->create($request->all());
-        return response()->json($fornecedor);
+
+        $validator = Validator::make($request->all(),
+            [
+                'nome'=>'required | max: 45',
+                'cnpj'=>'required | max: 10',
+                'email'=>'required|email|unique:fornecedor| max: 30' 
+            ]
+        );
+        
+        if($validator->fails()){
+            return response()->json(['error'=>'Erro ao validar os dados, tente novamente.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            
+        } else {
+            try{
+                $fornecedor = $this->model->create($request->all());
+                return response()->json($fornecedor, Response::HTTP_CREATED);
+            }catch (QueryException $exception){
+                return response()->json(['error'=>'Erro de conexão com o banco de dados'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }        
+    }
+
+    public function search ($id) {
+
+        $fornecedor = $this->model->find($id);
+        
+        try{
+            if(count($fornecedor)){
+                return response()->json($fornecedor, Response::HTTP_OK);
+            } else {
+                return response()->json(null, Response::HTTP_OK);
+            }
+        }catch (QueryException $exception){
+            return response()->json(['error'=>'Erro de conexão com o banco de dados'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function update(Request $request, $id) {
-        $fornecedor = $this->model->find($id)
-            ->update($request->all());
-        return response()->json($fornecedor);
+
+        $validator = Validator::make($request->all(),
+            [
+                'nome'=>'required | max: 45',
+                'cnpj'=>'required | max: 10',
+                'email'=>'required|email|unique:fornecedor| max: 30' 
+            ]
+        );
+        
+        if($validator->fails()){
+            return response()->json(['error'=>'Erro ao validar os dados, tente novamente.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            
+        } else {
+            try{
+                $fornecedor = $this->model->find($id)
+                    ->update($request->all());
+                return response()->json($fornecedor, Response::HTTP_OK);
+            }catch (QueryException $exception){
+                return response()->json(['error'=>'Erro de conexão com o banco de dados'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
     }
 
     public function delete($id) {
         $fornecedor = $this->model->find($id)
             ->delete();
-        return response()->json($fornecedor);
+
+        try{
+            return response()->json(null, Response::HTTP_OK);
+        }catch (QueryException $exception){
+            return response()->json(['error'=>'Erro de conexão com o banco de dados'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
